4a9479432e20d5d198c4f25f0560f819d9dee77c
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-03-10 11:00:13 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+$('.back').hide();
+
+
+$('front', '.flashcard').hover(function(){
+    $(this).hide();
+    $(this).siblings('.back').addClass(
+        "animated flipInY fast").show();
+});
+$()
+
+
+$('.back', '.flashcard').mouseleave(function(){
+    $(this).hide();
+    $(this).siblings('.front').addClass(
+        "animated flipInY fast").show();
+});
